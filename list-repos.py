3c47d4841c02ec2fa4ab5eb2b13a07318e86defe
Casey Vega <cvega@users.noreply.github.com>
2022-01-13 20:50:27 -0800

    Create list-repos.py

---
+++ list-repos.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import os
+import requests
+import sys
+
+
+def list_repos(org):
+    resp = requests.get(f"{url}/orgs/{org}/repos?per_page=100&page=1", headers=headers)
+    repos = resp.json()
+    while "next" in resp.links.keys():
+        resp = requests.get(resp.links["next"]["url"], headers=headers)
+        repos.extend(resp.json())
+
+    with open(f"{org}.txt", mode="w") as file:
+        for repo in repos:
+            if not repo["archived"] and not repo["fork"]:
+                file.write(f'{repo["full_name"]}\n')
+
+                
+if __name__ == "__main__":
+    if sys.argv[1] == "--help" or sys.argv[1] == "-h":
+        print(
+            f"""
+        Usage: {sys.argv[0]} ][-h] <org>\n
+        List repos that are not forks or archived and write to a file (<org>.txt)\n
+        Options:
+        -h | --help                  this message
+        """
+        )
+        exit()
+    url = os.getenv("GITHUB_API_ENDPOINT")
+    gql = os.getenv("GITHUB_GRAPHQL_ENDPOINT")
+    pat = os.getenv("GITHUB_TOKEN")
+    headers = {
+        "Authorization": f"token {pat}",
+        "Accept": "application/vnd.github.v3+json",
+    }
+    list_repos(sys.argv[1])
